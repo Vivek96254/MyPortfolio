4c811338b934778e230db4b325e72f14c5e1107a
Vivek Kumar Vinay <vivekvinay96254@gmail.com>
2025-03-31 14:01:15 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/viveklogo.png" sizes="any" />
+        <link rel="icon" href="/viveklogo.jpg" sizes="any" />
       </head>
       <body className={inter.className}>
         <ThemeProvider
